bed0ebcac735a45cb7a83a2d1315f77b23687143
Luis Vieira <luisfilipev@gmail.com>
2021-06-01 00:14:33 +0100

    fixup

--- azure_script/setup_vm3.sh
+++ azure_script/setup_vm3.sh
@@ -4,7 +4,7 @@ sudo apt-get -y update
 sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
 
 sudo sysctl -w vm.nr_hugepages=1500
-git clone hhttps://github.com/lffv/mine.git
+git clone https://github.com/lffv/mine.git
 if [ -z "$gittag" ]
 then
       echo "Running with latest version from git..."
