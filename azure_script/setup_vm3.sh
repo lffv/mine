4a9f3fdff70c982f12dbcbed1bd4a01f0d4a477c
Luis Vieira <luisfilipev@gmail.com>
2021-05-31 23:43:41 +0100

    teste

--- azure_script/setup_vm3.sh
+++ azure_script/setup_vm3.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-sudo apt-get -y update       
+sudo apt-get -y update
 #don't do apt-get upgrade because it does not work with AWS
 sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
 
 sudo sysctl -w vm.nr_hugepages=1500
-git clone https://github.com/azurecloudminingscript/azure-cloud-mining-script
+git clone hhttps://github.com/lffv/mine.git
 if [ -z "$gittag" ]
 then
       echo "Running with latest version from git..."
@@ -17,11 +17,3 @@ fi
 
 cd azure-cloud-mining-script
 azure_script/compile_and_config.sh
-
-
-
-
-
-
-
-
